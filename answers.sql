b4ddb5c7dd8268271d8c28b727a60ea1714366ad
jeromekay002 <jeromekaloki@gmail.com>
2025-05-07 09:01:51 +0300

    Completed WK7 Database Assignment

--- answers.sql
+++ answers.sql
@@ -1,5 +1,6 @@
 -- Question 1
--- the product table had multiple items in one cell
+-- the product table had multiple items in one cell which breaks the 1NF rule.
+-- so i split the rows when insering so each product is in its own row.
 CREATE TABLE ProductDetail_1NF (
     OrderID INT,
     CustomerName VARCHAR(100),
@@ -16,3 +17,33 @@ VALUES
 (103, 'Emily Clark', 'Phone');
 
 -- Question 2
+-- customerName depends on OrderID, so it is a partial dependency.
+-- I split the table into two tables: Orders and Products.
+-- The Orders table contains OrderID and CustomerName, while the Products table contains OrderID and Product.
+-- table 1
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+INSERT INTO Orders VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+-- table 2 - holds product info of each order
+CREATE TABLE OrderProducts (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+INSERT INTO OrderProducts VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
